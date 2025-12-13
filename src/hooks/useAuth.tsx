78a9c0f52ba9e5eef63dbd2dd451d7414901aa7c
Roberto Griel Filho <roberto@griel.com.br>
2025-12-13 18:15:07 -0300

    Adding: Implementing authentication context and provider

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,87 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react'
+import { useEncrypt } from './useEncrypt'
+import { api } from '../services/api'
+import Cookies from 'js-cookie'
+import { useRouter } from 'next/router'
+import { CookiesEnum } from '../database/Cookies'
+
+interface UserProps {
+  id: number
+  email: string
+}
+
+interface LoginProps {
+  user: UserProps
+  token: string
+}
+
+interface AuthContextType {
+  login: (email: string, password: string) => Promise<LoginProps>
+  checkLogin: () => Promise<void>
+}
+
+const AuthContext = createContext<AuthContextType>({} as AuthContextType)
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const Encrypt = useEncrypt()
+  const Router = useRouter()
+  const [userData, setUserData] = useState<UserProps | null>(null)
+
+  useEffect(() => {
+    checkLogin()
+  }, [Router.pathname])
+
+  async function checkLogin() {
+    if (!Router.pathname.startsWith('/dashboard')) return
+
+    const tokenFromCookies = Cookies.get(CookiesEnum.AUTH_TOKEN)
+
+    const shouldReturnToLogin =
+      !tokenFromCookies && Router.pathname.startsWith('/dashboard') && Router.pathname !== '/dashboard/login'
+
+    if (shouldReturnToLogin) {
+      Router.push('/dashboard/login')
+      return
+    }
+
+    if (tokenFromCookies && !userData) {
+      const result = await api.get<UserProps>('/me', {
+        headers: {
+          Authorization: `Bearer ${tokenFromCookies}`
+        }
+      })
+
+      setUserData(result.data)
+    }
+  }
+
+  async function login(email: string, password: string) {
+    const passwordEnc = await Encrypt.encrypt(password)
+
+    const body = {
+      email,
+      password: passwordEnc
+    }
+
+    const response = await api.post<LoginProps>('/login', body)
+
+    Cookies.set(CookiesEnum.AUTH_TOKEN, response.data.token, { expires: 30 })
+
+    return response.data
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        login,
+        checkLogin
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
