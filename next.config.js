a12bee02554ecf9dd68f876641f658bbe219eb31
Roberto Griel Filho <roberto@griel.com.br>
2025-12-13 18:04:36 -0300

    Updating: Adding SHARED_SECRET to environment variables in config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   env: {
     BIRTH_DATE: process.env.BIRTH_DATE,
     DOMAIN_CHECK_API: process.env.DOMAIN_CHECK_API,
-    BACKEND_API: process.env.BACKEND_API
+    BACKEND_API: process.env.BACKEND_API,
+    SHARED_SECRET: process.env.SHARED_SECRET
   },
   compiler: {
     styledComponents: true
