5c9fa0e8b79b24535705cfdeee736eb5b1768f2c
Roberto Griel Filho <roberto@griel.com.br>
2025-12-13 12:26:26 -0300

    Updating environment variables in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    BIRTH_DATE: process.env.BIRTH_DATE
+    BIRTH_DATE: process.env.BIRTH_DATE,
+    DOMAIN_CHECK_API: process.env.DOMAIN_CHECK_API,
+    BACKEND_API: process.env.BACKEND_API
   },
   compiler: {
     styledComponents: true
